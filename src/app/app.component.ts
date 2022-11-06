003c1b6b83998216cfc9cb091b0171f139b5153f
DyQ3HELOF1 <87905975+DyQ3HELOF1@users.noreply.github.com>
2022-11-06 23:33:22 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PrimeService} from "./prime.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'euler-three';
+export class AppComponent implements OnInit{
+
+  public myNumber: number; // 1 < myNumber (int) < 799993^2
+  public largestPrimeFactor: number;
+
+  constructor(private primeService: PrimeService) {}
+
+  ngOnInit(): void {
+    this.initializeDefaults();
+    let nr = this.myNumber;
+    let prime = 2;
+    while (prime <= Math.sqrt(nr)) {
+      nr = this.divideOut(nr, prime);
+      prime = this.primeService.nextPrime(prime);
+    }
+    this.largestPrimeFactor = nr === 1 ? this.primeService.previousPrime(prime) : nr;
+  }
+
+  private initializeDefaults() {
+    this.myNumber = 600851475143;
+  }
+
+  private divideOut(nr: number, prime: number): number {
+    while (nr % prime === 0) {
+      nr = nr / prime;
+    }
+    return nr;
+  }
 }
